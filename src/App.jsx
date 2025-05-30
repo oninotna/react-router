1649da6fb78218cade1766008cc63ba8418d204e
oninotna <antoninomusarratubbi@gmail.com>
2025-05-30 13:54:40 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <><h1 className='text-center'>boilerplate</h1></>
+  )
+}
