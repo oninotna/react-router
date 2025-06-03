f693fffa8282d2fc51297914804a4a6a53c5edbe
oninotna <antoninomusarratubbi@gmail.com>
2025-06-03 17:51:14 +0200

    add single post component

--- src/pages/posts/SinglePost.jsx
+++ src/pages/posts/SinglePost.jsx
@@ -1,17 +1,30 @@
 import axios from "axios";
+import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const urlApi = "http://localhost:3000/posts";
 
 export default function SinglePost () {
-    const [dataPosts, setDataPosts] = useState([]);
+    const [dataPost, setDataPost] = useState({});
+    const {id} = useParams();
+    console.log(id);
+    
 
     useEffect(() => {
-        axios.get(urlApi).then((res) => {
+        axios.get(`${urlApi}/${id}`).then((res) => {
           console.log(res.data);  
-          setDataPosts(res.data);
+          setDataPost(res.data);
         });
     }, []);
 
+    return (
+        <div className="container">
+            <h1>{dataPost.titolo}</h1>
+            <img src={dataPost.immagine} alt={dataPost.titolo} />
+            <p>{dataPost.contenuto}</p>
+            <p>{dataPost.tags}</p>
+            <Link className="btn btn-secondary" to="/posts">Torna alla lista dei post</Link>
+        </div>
+    )
 }
